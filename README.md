e2b3488d0d9b915eae6574707eedbfa5ffc397a7
gamemaster 2b <gamemaster2b@gmail.com>
2020-08-06 19:20:13 +0000

    First make

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-# temperature-interchange
+## temperature-interchange ##
 change temperature between ℃ and ℉
 [![Run on Repl.it](https://repl.it/badge/github/gamemaster2b/temperature-interchange)](https://repl.it/github/gamemaster2b/temperature-interchange)
 ℃=(℉-32)/1.8
 [This is the github repository.](https://github.com/gamemaster2b/temperature-interchange)
+## Progress
+- [x] [`create the cpp file`](temp.cpp)
+- [x] `write the lines of code`
+- [x] `debug the code`
---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+double FtoC(double temp){
+  return (temp-32)/1.8;
+}
+double CtoF(double temp){
+  return (temp*1.8)+32;
+}
+int main(){
+  std::cout<<"To change Degrees Celcius to Dregrees Fehreinheit enter 'f'.\n"<<"To change Degrees Fehreinheit to Dregrees Celcius enter 'c'.\n";
+  char str;
+  double temp;
+  std::cout<<"What are you going to do:";
+  std::cin>>str;
+  if(str=='f'){
+    std::cout<<"Enter tempreture:";
+    std::cin>>temp;
+    std::cout<<"The temperature is "<<FtoC(temp)<<"℃\n";
+  }
+  else if(str=='c'){
+    std::cout<<"Enter tempreture:";
+    std::cin>>temp;
+    std::cout<<"The temperature is "<<CtoF(temp)<<"℉\n";
+  }
+  else{
+    std::cout<<"You are full of ..., just try again.\n";
+    main();
+  }
+  return 0;
+}
